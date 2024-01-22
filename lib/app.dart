d9880b3deae3721568c78ddb370018f1f16bfa44
김성중 <ksj@MacBookAir.local>
2024-01-22 21:25:35 +0900

    :lipstick: Add Getx, GetMaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,21 +1,21 @@
+import 'package:clone_app/view/s_login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:clone_app/view/s_main.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Clone App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepPurple,
-        ),
+        primaryColor: Colors.black,
         useMaterial3: true,
       ),
-      home: const MainScreen(),
+      home: const LoginPage(),
     );
   }
 }
